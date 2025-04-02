d2178d2d4c5671b8fd20ceefa0bf3bce40892e6c
HellcatOG <pranavunnikrishnan109@gmail.com>
2025-04-02 22:23:24 +0530

    DB connected

---
+++ src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.ecommerce.project.exceptions;
+
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@ExceptionHandler
+public class MyGlobalExceptionHandler {
+
+
+
+}
