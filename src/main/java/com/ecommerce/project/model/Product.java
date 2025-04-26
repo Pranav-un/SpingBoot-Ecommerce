4cb1aee7161a009b370b012d81716782b61a68f4
HellcatOG <pranavunnikrishnan109@gmail.com>
2025-04-26 21:01:40 +0530

    Started On Product Module

---
+++ src/main/java/com/ecommerce/project/model/Product.java
@@ -0,0 +1,26 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long productId;
+    private String productName;
+    private String description;
+    private Integer quantity;
+    private Double price;
+    private Double specialPrice;
+
+    @ManyToOne
+    @JoinColumn(name = "category_id")
+    private Category category;
+}
---
+++ src/main/java/com/ecommerce/project/repositories/ProductRepository.java
@@ -0,0 +1,10 @@
+package com.ecommerce.project.repositories;
+
+import com.ecommerce.project.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ProductRepository extends JpaRepository<Product, Long> {
+
+}
