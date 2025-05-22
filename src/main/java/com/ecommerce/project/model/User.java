94175e59e8300367d3c1377bb4575843ebbd902f
HellcatOG <pranavunnikrishnan109@gmail.com>
2025-05-22 13:33:46 +0530

    Users and Roles

---
+++ src/main/java/com/ecommerce/project/model/User.java
@@ -0,0 +1,68 @@
+package com.ecommerce.project.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "users",
+         uniqueConstraints = {@UniqueConstraint(columnNames = "username"),
+                              @UniqueConstraint(columnNames = "email")
+})
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private long userId;
+
+    @NotBlank
+    @Size(max = 20)
+    @Column(name = "username")
+    private String username;
+
+    @NotBlank
+    @Email
+    @Size(max = 50)
+    @Column(name = "email")
+    private String email;
+
+    @NotBlank
+    @Size(max = 120)
+    @Column(name = "password")
+    private String password;
+
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    @Getter
+    @Setter
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+        fetch = FetchType.EAGER)
+   @JoinTable(name = "user_role",
+                joinColumns = @JoinColumn(name = "user_id"),
+                inverseJoinColumns = @JoinColumn(name = "role_id"))
+
+    private Set<Role> roles = new HashSet<>();
+
+    @OneToMany(mappedBy =  "user",
+            cascade = {CascadeType.PERSIST, CascadeType.MERGE},
+            orphanRemoval = true)
+    private Set<Product> products;
+
+}
+
